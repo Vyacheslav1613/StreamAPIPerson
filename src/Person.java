dfbce9ea94628f8ed180ef2762d2c026e475e464
Вячелав <ldobryakl@yandex.ru>
2023-09-19 13:21:33 +0500

    First commit

---
+++ src/Person.java
@@ -0,0 +1,44 @@
+class Person {
+    private final String name;
+    private final String family;
+    private final Integer age;
+    private final Sex sex;
+    private final Education education;
+
+    public Person(String name, String family, int age, Sex sex, Education education) {
+        this.name = name;
+        this.family = family;
+        this.age = age;
+        this.sex = sex;
+        this.education = education;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public Education getEducation() {
+        return education;
+    }
+
+    @Override
+    public String toString() {
+        return '\n' + "Фамилия: " + family +
+                ", Имя: " + name +
+                ", возраст: " + age +
+                ", пол: " + sex +
+                ", образование: " + education;
+    }
+}
